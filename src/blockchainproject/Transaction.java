4b4c3883d55b327a3797a2e12fed07dd4c70adcd
Max af Klercker <cobblegenerator@gmail.com>
2021-05-27 18:12:05 +0200

    Transaction signing

--- src/blockchainproject/Transaction.java
+++ src/blockchainproject/Transaction.java
@@ -5,12 +5,25 @@
  */
 package blockchainproject;
 
+import java.security.*;
+
 /**
- *
+ * https://docs.oracle.com/javase/tutorial/security/apisign/step3.html
  * @author max.afklercker
  */
 public class Transaction {
     private int amount;
-    private String belongsTo;
-    private String sentFrom;
+    private PublicKey belongsTo;
+    private PublicKey sentFrom;
+    private String transactionHash;
+
+    public Transaction(int amount, PublicKey belongsTo, PublicKey sentFrom, PrivateKey privateKey) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidKeyException {
+        this.amount = amount;
+        this.belongsTo = belongsTo;
+        this.sentFrom = sentFrom;
+        Signature dsa = Signature.getInstance("SHA1withDSA", "SUN"); 
+        dsa.initSign(privateKey);
+    }
+    
+    
 }
